fd03ef45d115d12af787cea8860330b6dae26f61
AquaHaze <142215610+AquaHaz3@users.noreply.github.com>
2024-09-20 09:05:07 +0500

    0

--- StackImpl/Main.cpp
+++ StackImpl/Main.cpp
@@ -13,49 +13,35 @@ bool check_sum_is_zero(const std::vector<int>& arr);
 int main() 
 {
 
-	stack_combined<int> s 
-		= stack_combined<int>(9, chunk_size_policy::EXP);
-
-	s.push(7);
-	s.push(4);
-	s.push(5);
-	s.push(3);
-	s.push(2);
-	s.push(1);
-	s.push(6);
-	s.push(0);
-
-	selection_sort<stack_combined<int>>(s);
-
-	while(s.size() > 0) 
-	{
-		std::cout << s.top() << std::endl;
-		s.pop();
-	};
-
+	
 	return 0;
 
-}
-
-/*
-@return
-1 - positive overflow, -1 negative overflow, 0 - no overflow
-*/
-int overflow_dir(int a, int b)
-{
-	int sum = a + b;
-	if (a > 0 && b > 0 && sum < a && sum < b) return  1;
-	if (a < 0 && b < 0 && sum > a && sum > b) return -1;
-	return 0;
 }
 
 bool check_sum_is_zero(const std::vector<int>& arr) 
 {
-	int sum = 0;
-	int cycles = 0;
+	stack_array positive = stack_array<int>(arr.size());
+	stack_array negative = stack_array<int>(arr.size());
+
 	for (int num : arr) {
-		cycles = cycles + overflow_dir(num, sum);
-		sum = sum + num;
+		if (num >= 0) positive.push(num);
+		else negative.push(num);
 	}
-	return (sum == 0 && cycles == 0);
+
+	int sum = 0;
+
+	while (positive.size() > 0 || negative.size() > 0) {
+		if (sum < 0) {
+			if (positive.size() == 0) return false;
+			sum += positive.top();
+			positive.pop();
+		}
+		else {
+			if (negative.empty()) return (sum == 0);
+			sum += negative.top();
+			negative.pop();
+		}
+	}
+
+	return false;
 } 
